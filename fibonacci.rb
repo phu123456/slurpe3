577470ddbddfadfe4f1e2e6aed3ecd290ad5dc9d
sakko <saklism@gmail.com>
2015-12-27 23:13:48 +0700

    implemented memoize

--- fibonacci.rb
+++ fibonacci.rb
@@ -1,6 +1,10 @@
 def fibonacci(n)
+  @memo ||= {}
   return n if (0..1).include?(n)
-  return (fibonacci(n-1)+fibonacci(n-2))
+  return @memo[n] unless @memo[n].nil?
+  puts "recalculate #{n}"
+  @memo[n] = (fibonacci(n-1)+fibonacci(n-2))
+  @memo[n]
 end
 
 puts fibonacci(15)
